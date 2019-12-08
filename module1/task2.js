1a58870d51acb6c37c44a6dde98af25c6c7da312
SeregaZnam <Sergey-znamensk@yandex.ru>
2019-12-08 19:36:07 +0400

    added ram

--- module1/task2.js
+++ module1/task2.js
@@ -1,22 +1,40 @@
 import csv from "csvtojson";
 import fs from 'fs';
+import { pipeline } from 'stream';
 
-const pathCSVFile = './csv/file.csv';
-const pathTXTFile = './txt/file.txt';
+if (process.argv[2] === 'ram') {
+  const pathCSVFileRAM = './csv/file.r.csv';
+  const pathTXTFileRAM = './txt/file.r.txt';
+  const readableStream = fs.createReadStream(pathCSVFileRAM);
+  const writableStream = fs.createWriteStream(pathTXTFileRAM);
 
-csv({
-  headers: ["book", "author", "amount", "price"],
-  includeColumns: /(book|author|price)/,
-  colParser: {
-    "book": "string",
-    "author": "string",
-    "price": "number"
-  }
-})
-  .fromFile(pathCSVFile)
-  .then((json) => {
-    let data = json.map((obj) => JSON.stringify(obj)).join('\n');
-    fs.writeFile(pathTXTFile, data, (error, data) => {
-      if(error) console.log(error)
+  csv({
+    headers: ["book", "author", "amount", "price"],
+    includeColumns: /(book|author|price)/,
+    colParser: {
+      "book": "string",
+      "author": "string",
+      "price": "number"
+    }
+  }).fromFile(pathCSVFileRAM).pipe(writableStream);
+} else {
+  const pathCSVFile = './csv/file.csv';
+  const pathTXTFile = './txt/file.txt';
+  
+  csv({
+    headers: ["book", "author", "amount", "price"],
+    includeColumns: /(book|author|price)/,
+    colParser: {
+      "book": "string",
+      "author": "string",
+      "price": "number"
+    }
+  })
+    .fromFile(pathCSVFile)
+    .then((json) => {
+      let data = json.map((obj) => JSON.stringify(obj)).join('\n');
+      fs.writeFile(pathTXTFile, data, (error, data) => {
+        if(error) console.log(error)
+      });
     });
-  });
+}
