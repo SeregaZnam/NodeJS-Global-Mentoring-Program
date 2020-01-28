b4118a77affaab644372decc017bce8ba6298ade
Sergei Michkov <Sergei_Michkov@epam.com>
2020-01-29 00:50:21 +0400

    added sequelize methods

--- module2/database/entities/User.ts
+++ module2/database/entities/User.ts
@@ -1,7 +1,7 @@
-import {DataTypes} from "sequelize";
+import {DataTypes, ModelCtor, Model} from "sequelize";
 import {sequelize} from "../database";
 
-const User = sequelize.define('User', {
+sequelize.define('User', {
   id: {
     type: DataTypes.UUID,
     allowNull: false,
@@ -24,33 +24,4 @@ const User = sequelize.define('User', {
   timestamps: false
 });
 
-export const UserModel = sequelize.models.User;
-
-// import {sequelize} from "../database";
-// import Sequelize, {Model} from "sequelize";
-//
-// class UserModel extends Model {}
-//
-// UserModel.init({
-//   id: {
-//     type: Sequelize.UUID,
-//     allowNull: false,
-//     primaryKey: true,
-//     defaultValue: Sequelize.UUID
-//   },
-//   login: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   },
-//   password: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   },
-//   age: {
-//     type: Sequelize.INTEGER
-//   }
-// }, {
-//   sequelize
-// });
-//
-// export const userModel = sequelize.models.UserModel;
+export default sequelize.models.User as ModelCtor<Model>;
