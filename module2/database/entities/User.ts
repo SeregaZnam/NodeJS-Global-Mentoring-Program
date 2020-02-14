86280ae96391237276ea300cd831efe3fc63d507
Sergei Michkov <sergei_michkov@epam.com>
2020-02-14 11:32:22 +0400

    develop

--- module2/database/entities/User.ts
+++ module2/database/entities/User.ts
@@ -1,7 +1,7 @@
 import {DataTypes, ModelCtor, Model} from "sequelize";
 import {sequelize} from "../database";
 
-sequelize.define('User', {
+const User: any = sequelize.define('User', {
   id: {
     type: DataTypes.UUID,
     allowNull: false,
@@ -24,12 +24,12 @@ sequelize.define('User', {
   timestamps: false
 });
 
-sequelize.define('Group', {
+const Group: any = sequelize.define('Group', {
   id: {
      type: DataTypes.UUID,
      allowNull: false,
      primaryKey: true,
-     defaultValue: DataTypes.UUID
+     defaultValue: DataTypes.UUIDV4
   },
   name: {
      type: DataTypes.STRING,
@@ -44,42 +44,79 @@ sequelize.define('Group', {
   timestamps: false
 });
 
-sequelize.define('UserGroup', {
-  userId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-    references: {
-      model: 'User',
-      key: 'id'
-    }
-  },
-  groupId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-    references: {
-      model: 'Group',
-      key: 'id'
-    }
-  }
-}, {
-  tableName: "usergroup",
-  timestamps: false
+// sequelize.define('UserGroup', {
+//   userId: {
+//     type: DataTypes.UUID,
+//     allowNull: false,
+//     references: {
+//       model: 'User',
+//       key: 'id'
+//     }
+//   },
+//   groupId: {
+//     type: DataTypes.UUID,
+//     allowNull: false,
+//     references: {
+//       model: 'Group',
+//       key: 'id'
+//     }
+//   }
+// }, {
+//   tableName: "usergroup",
+//   timestamps: false
+// });
+
+sequelize.models.User.belongsToMany(Group, {
+  through: 'UserGroup'
+  // as: 'users',
+  // foreignKey: 'userId',
+  // otherKey: 'groupId'
 });
 
-sequelize.models.User.belongsToMany(sequelize.models.Group, {
-  through: sequelize.models.UserGroup,
-  as: 'users',
-  foreignKey: 'userId',
-  otherKey: 'groupId'
+sequelize.models.Group.belongsToMany(User, {
+   through: 'UserGroup',
+  //  as: 'groups',
+  //  foreignKey: 'groupId',
+  //  otherKey: 'userId'
 });
 
-sequelize.models.Group.belongsToMany(sequelize.models.User, {
-   through: sequelize.models.UserGroup,
-   as: 'groups',
-   foreignKey: 'groupId',
-   otherKey: 'userId'
-});
+(async () => {
 
-sequelize.sync({force: true});
+  await sequelize.sync();
+
+  // User.create({
+  //   "login": "Alex",
+  //   "password": "123test",
+  //   "age": 15
+  // });
+
+  // User.create({
+  //   "login": "Robin",
+  //   "password": "asd456A",
+  //   "age": 20
+  // });
+  // "041b7ddb-0e7e-41cc-b7e2-c51ec8e5c0bc"
+
+  // const group1 = await Group.create({
+  //   name: 'Group1',
+  //   permissions: ['READ']
+  // })
+
+  // group1.setUsers([
+  //   "041b7ddb-0e7e-41cc-b7e2-c51ec8e5c0bc",
+  //   "d254c057-eba8-4c30-b96a-d01d6d837765"
+  // ])
+
+  // User.destroy({
+  //   where: {id: "041b7ddb-0e7e-41cc-b7e2-c51ec8e5c0bc"}
+  // });
+
+})();
+
+// sequelize.models.User.bulkCreate([{
+//   login: 'Alex',
+//   password: '123test',
+//   age: 15
+// }])
 
 export default sequelize.models.User as ModelCtor<Model>;
